08b9afcff73c5b9ee973dea6d140f803d403861e
akki2825 <akhilesh.rocking@gmail.com>
2019-11-19 20:38:14 +0530

    add transformer script that transforms data formats

---
+++ lglint/transformers.py
@@ -0,0 +1,20 @@
+from typing import Dict
+import spacy
+
+nlp = spacy.load("en_blackstone_proto")
+
+
+def convert_to_cheyyali_format(text: str) -> Dict:
+    """
+    convert texts to jsonl format for cheyyali annotator
+    """
+    doc = nlp(text)
+    ent_dict = {}
+    all_labels = []
+    for ent in doc.ents:
+        label = [ent.start_char, ent.end_char, ent.label_]
+        all_labels.append(label)
+    ent_dict["text"] = (text,)
+    ent_dict["labels"] = all_labels
+
+    return ent_dict
