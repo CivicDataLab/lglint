f427e7acda6db2cfadcc0cf160c93ddc619ddcda
DC <eopoxf@gmail.com>
2020-01-09 19:23:01 +0530

    Fix filenames

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 
-from scripts.ConcatenateMetadataOfState import ConcatenateMetaDataOfState
-from scripts.CopyFilteredJudgements import CopyFilteredJudgements
-from scripts.FilterByCaseType import FilterByCaseType
-from scripts.Pdf2Txt import Pdf2Txt
-from scripts.SampleNCases import SampleNCases
+from scripts.concatenate_metadata_of_state import ConcatenateMetaDataOfState
+from scripts.copy_filtered_judgements import CopyFilteredJudgements
+from scripts.filter_by_case_type import FilterByCaseType
+from scripts.pdf_to_txt import Pdf2Txt
 from scripts.pipeline import Pipeline
+from scripts.sample_n_cases import SampleNCases
 
 pipeline = Pipeline()
 
@@ -15,7 +15,8 @@ pipeline \
                                     out_file='delhi_metadata.csv')) \
     .add(FilterByCaseType(case_type=['SC', 'Cr. Case', 'CC'])) \
     .add(SampleNCases(number_of_cases=30)) \
-    .add(CopyFilteredJudgements(source='/home/dc/Documents/civicdatalab/judiciary/data/pocso/', destination=os.path.join(os.path.dirname(__file__), 'input'))) \
+    .add(CopyFilteredJudgements(source='/home/dc/Documents/civicdatalab/judiciary/data/pocso/',
+                                destination=os.path.join(os.path.dirname(__file__), 'input'))) \
     .add(Pdf2Txt(os.path.join(os.path.dirname(__file__), 'input'), os.path.join(os.path.dirname(__file__), 'out')))
 
 pipeline.execute()
