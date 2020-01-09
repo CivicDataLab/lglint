3cbdf6466e0f454faec738f56b5ebd3f94610a7c
DC <eopoxf@gmail.com>
2020-01-09 21:02:15 +0530

    Extract condition to variable

--- scripts/copy_filtered_judgements.py
+++ scripts/copy_filtered_judgements.py
@@ -21,6 +21,7 @@ class CopyFilteredJudgements(Task):
         # for ci in case_numbers:
         for root, dirs, files in os.walk(self.source):
             for file in files:
-                if file.endswith('.pdf') and case_numbers.__contains__(file.split('_')[0]):
+                is_judgement_in_required_cases = case_numbers.__contains__(file.split('_')[0])
+                if file.endswith('.pdf') and is_judgement_in_required_cases:
                     path_file = os.path.join(self.destination, file)
                     copyfile(os.path.join(root, file), path_file)
