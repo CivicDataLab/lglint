ce8d8ca0969af39abffacab51e1214411bea365c
DC <eopoxf@gmail.com>
2020-01-09 20:35:30 +0530

    Add config

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+from simple_config import Config
+
+default_settings = {
+    "base_data_dir": '/path/to/base_data_dir',
+    'state_to_analyse': 'state name',
+    'number_of_sample_judgements': 30,
+    'judgements_case_types': ['case_type'],
+    'combined_metadata_file': 'metadata.csv',
+    "all_judgements_pdf_directory": "input",
+    "all_judgements_txt_directory": "output"
+}
+config_file = os.path.join(os.path.dirname(__file__), 'config.json')
+settings = Config(config_file, defaults=default_settings)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+from config import settings
 from scripts.concatenate_metadata_of_state import ConcatenateMetaDataOfState
 from scripts.copy_filtered_judgements import CopyFilteredJudgements
 from scripts.filter_by_case_type import FilterByCaseType
@@ -9,14 +10,15 @@ from scripts.sample_n_cases import SampleNCases
 
 pipeline = Pipeline()
 
+all_judgements_pdf_directory = os.path.join(os.path.dirname(__file__), settings.all_judgements_pdf_directory)
+all_judgements_txt_directory = os.path.join(os.path.dirname(__file__), settings.all_judgements_txt_directory)
 pipeline \
-    .add(ConcatenateMetaDataOfState(base_data_dir="/home/dc/Documents/civicdatalab/judiciary/data/pocso/",
-                                    state='Delhi',
-                                    out_file='delhi_metadata.csv')) \
-    .add(FilterByCaseType(case_types=['SC', 'Cr. Case', 'CC'])) \
-    .add(SampleNCases(number_of_cases=30)) \
-    .add(CopyFilteredJudgements(source='/home/dc/Documents/civicdatalab/judiciary/data/pocso/',
-                                destination=os.path.join(os.path.dirname(__file__), 'input'))) \
-    .add(Pdf2Txt(os.path.join(os.path.dirname(__file__), 'input'), os.path.join(os.path.dirname(__file__), 'out')))
+    .add(ConcatenateMetaDataOfState(base_data_dir=settings.base_data_dir,
+                                    state=settings.state_to_analyse,
+                                    out_file=settings.combined_metadata_file)) \
+    .add(FilterByCaseType(case_types=settings.judgements_case_types)) \
+    .add(SampleNCases(number_of_cases=settings.number_of_sample_judgements)) \
+    .add(CopyFilteredJudgements(source=settings.base_data_dir, destination=all_judgements_pdf_directory)) \
+    .add(Pdf2Txt(all_judgements_pdf_directory, all_judgements_txt_directory))
 
 pipeline.execute()
