1a863ba8abce11b85ffb67d4ef46a1705fe913f4
DC <eopoxf@gmail.com>
2020-01-13 16:35:40 +0530

    rename variables

--- scripts/pdf_to_txt.py
+++ scripts/pdf_to_txt.py
@@ -15,15 +15,15 @@ class Pdf2Txt(Task):
     def execute(self):
         for filename in os.listdir(self.input_dir):
             if filename.endswith(".pdf"):
-                ofile = filename.split(".")[0] + ".txt"
+                out_file = filename.split(".")[0] + ".txt"
                 if self.output_dir:
-                    ofile = os.path.join(self.output_dir, ofile)
-                ifile = os.path.join(self.input_dir, filename)
-                with open(ifile, "rb") as input_file:
+                    out_file = os.path.join(self.output_dir, out_file)
+                in_file = os.path.join(self.input_dir, filename)
+                with open(in_file, "rb") as input_file:
                     try:
                         pdf = pdftotext.PDF(input_file, "UTF-8")
-                        with open(ofile, "w+") as out_file:
+                        with open(out_file, "w+") as out_file:
                             text = "".join(pdf)
                             out_file.write(text)
                     except pdftotext.Error:
-                        print('the file {} looks corrupted'.format(ifile))
+                        print('the file {} looks corrupted'.format(in_file))
