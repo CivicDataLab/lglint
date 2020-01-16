fb1563e3fe1fa168efa3037f518a7ef87867f84d
DC <eopoxf@gmail.com>
2020-01-16 16:23:11 +0530

    add metadata to the data

--- scripts/convert_to_cheyyali_format.py
+++ scripts/convert_to_cheyyali_format.py
@@ -2,6 +2,8 @@ import json
 import os
 from typing import List, Dict, Mapping
 
+import pandas as pd
+
 from scripts.task import Task
 
 
@@ -14,15 +16,20 @@ class ConvertToCheyyaliFormat(Task):
 
     def _execute(self):
         for filename in os.listdir(self.input_dir):
+            assert isinstance(self.shared_resource, pd.DataFrame)
             if filename.endswith(".txt"):
                 judgement = os.path.join(self.input_dir, filename)
                 with open(judgement, "r") as input_file:
                     input_data = input_file.read()
-                    cheyalli_data = self._convert_to_cheyyali_format(input_data)
+                    case_number = int(filename.split('_')[0])
+                    case = self.shared_resource[self.shared_resource['case_no'] == case_number]
+                    metadata = json.loads(case.to_json(orient='records'))[0]
+                    cheyalli_data = self._convert_to_cheyyali_format(input_data, metadata)
+
                     cheyyali_file = os.path.join(self.input_dir, self.out_file)
                     with open(cheyyali_file, "a") as f:
                         f.writelines([json.dumps(cheyalli_data) + '\n'])
 
     @staticmethod
-    def _convert_to_cheyyali_format(input_text) -> Dict[str, Mapping[str, List[List[str]]]]:
-        return {'text': input_text, "labels": []}
+    def _convert_to_cheyyali_format(input_text, metadata: dict) -> Dict[str, Mapping[str, List[List[str]]]]:
+        return {'text': input_text, "labels": [], 'meta': metadata}
