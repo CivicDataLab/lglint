92ee1a160bfdc12c9f53eaf84c963905da40f808
DC <eopoxf@gmail.com>
2020-01-16 10:55:02 +0530

    cast casenumber to int

--- scripts/copy_filtered_judgements.py
+++ scripts/copy_filtered_judgements.py
@@ -21,7 +21,10 @@ class CopyFilteredJudgements(Task):
         # for ci in case_numbers:
         for root, dirs, files in os.walk(self.source):
             for file in files:
-                is_judgement_in_required_cases = case_numbers.__contains__(file.split('_')[0])
-                if file.endswith('.pdf') and is_judgement_in_required_cases:
-                    path_file = os.path.join(self.destination, file)
-                    copyfile(os.path.join(root, file), path_file)
+                if file.endswith('.pdf'):
+                    try:
+                        if '_' in file and int(file.split('_')[0]) in case_numbers:
+                            path_file = os.path.join(self.destination, file)
+                            copyfile(os.path.join(root, file), path_file)
+                    except Exception as e:
+                        print(e.__str__() + "for file" + root + '/' + file)
